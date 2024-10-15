e417d6a38cb88e9bc270582d91d0ed5b56327a29
y-jatin <126376762+y-jatin@users.noreply.github.com>
2024-10-15 18:13:21 +0530

    Uploaded changes

---
+++ real estate price predictor/server/util.py
@@ -0,0 +1,44 @@
+import json
+import pickle
+import numpy as np
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location, sqft, bath, bhk):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    X = np.zeros(len(__data_columns))
+    X[0] = bath
+    X[1] = sqft
+    X[2] = bhk
+    if loc_index >= 0:
+        X[loc_index] = 1
+
+    return round(__model.predict([X])[0], 2)
+
+def get_location_names():
+    return __locations
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+
+    with open("./artifacts/pune_home_prices_model.pickle", 'rb') as f:
+        __model = pickle.load(f)
+    print("loading saved artifacts...done")
+
+if __name__ == "__main__":
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('Sinhagad Road', 1000, 2, 2))
